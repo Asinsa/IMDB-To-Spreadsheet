b69be5c9c7e2ee13a8b87ba229f8dd3f0b55286e
Asinsa <asinsa.g@gmail.com>
2021-07-11 16:29:34 +0100

    Accessing the google sheet

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import requests
 from pprint import PrettyPrinter
 
 pp = PrettyPrinter()
+
+# Authorize the google sheets API
+scope = [
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file"
+    ]
+
+file_name = "api_keys/client_key.json"
+creds = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
+client = gspread.authorize(creds)
+
+# Fetch & print everything in the google sheet
+sheet = client.open("Films, TV Shows and Books").sheet1
+python_sheet = sheet.get_all_records()
+pp.pprint(python_sheet)
+
+# OMDB Api Key
 file = open("api_keys/apiKey.txt", 'r')
 apiKey = file.readline(20)
 
-#Fetch Movie Data
-data_URL = 'http://www.omdbapi.com/?apikey='+apiKey
-year = ''
-movieTitle = 'Fast & Furious'
+# Fetch Movie Data from OMDB
+data_URL = "http://www.omdbapi.com/?apikey=" + apiKey
+year = ""
+movieTitle = "Fast & Furious"
 params = {
     's':movieTitle,
     'type':'movie',
